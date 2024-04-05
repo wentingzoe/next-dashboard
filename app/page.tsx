c185b2ab28b8247cbf570c14df5e85ec829fb80b
Wenting Yong <wyong@orangerine.com>
2024-04-05 15:52:08 -0400

    Added image into home page.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
         {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
         {/* Tailwind classname */}
         <div className={styles.shape} />
