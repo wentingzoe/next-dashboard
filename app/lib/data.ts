86ae515f830af6d6712ca3e046c6cab19aac8915
Wenting Yong <wyong@orangerine.com>
2024-04-09 16:44:25 -0400

    Added comment about Parallel data.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -69,6 +69,7 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+    // promise.all() returns an array of results in the same order as the promises
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
