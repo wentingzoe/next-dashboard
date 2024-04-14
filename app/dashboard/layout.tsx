985a13fb6addb0ef8ea5783ecb67b4e6441e25c5
Wenting Yong <wyong@orangerine.com>
2024-04-14 14:09:09 -0400

    Added the comment about static and dynamic components of the application.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,9 +4,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
+        {/* SideNav is static */}
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {/* Children are dynamic  */}
+        {children}
+      </div>
     </div>
   );
 }
