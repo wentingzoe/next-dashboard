5cd91462edc7724fa65eecbdde41403489969d58
Wenting Y <ywtzoe@gmail.com>
2024-04-17 21:08:10 -0400

    Extracted data from formData which linked create-form.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,12 @@
 'use server';
+//similar state management logic can be used in the server-side code.
  
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+	  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Test it out:
+  console.log(rawFormData);
+}
