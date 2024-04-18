5fcd75245a077a6b742deda62270ba572d6276dd
Wenting Yong <wyong@orangerine.com>
2024-04-17 21:19:19 -0400

    Using Zod to validate data and added cents and date to formSchema.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,26 @@
 'use server';
 //similar state management logic can be used in the server-side code.
- 
+
+import {z} from 'zod';
+//`zod` is a TypeScript-first schema declaration and validation library.
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
-	  const rawFormData = {
+	const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+	
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split('T')[0];
   // Test it out:
-  console.log(rawFormData);
+  console.log(customerId, amount, status,amountInCents,date);
+	console.log(typeof amount);
 }
