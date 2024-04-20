acaf2d8cc7f1a78c93315ffda87c80600e78d5e0
Wenting Y <ywtzoe@gmail.com>
2024-04-20 16:43:05 -0400

    Refactor deleteInvoice function to include error handling and return message

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,6 +64,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+    try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
