c00270ffbcba66d5b1cc374a31435c5f1ec7299e
Wenting Yong <wyong@orangerine.com>
2024-04-20 16:41:44 -0400

    Fix error handling in createInvoice function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,11 +26,16 @@ export async function createInvoice(formData: FormData) {
 	
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split('T')[0];
-	 
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+	
+  try{
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Create Invoice.',
+       };
+    }
 	revalidatePath('/dashboard/invoices');
 	redirect('/dashboard/invoices');
 }
