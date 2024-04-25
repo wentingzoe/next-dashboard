b44a7faaeedb472d92bc29d211edbba8658ca767
Wenting Y <ywtzoe@gmail.com>
2024-04-25 10:46:54 -0400

    Add error message for create form in invoices

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -132,6 +132,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
           </div>
         </fieldset>
+        {/* Error Message */}
         <div aria-live="polite" aria-atomic="true">
           {state.message ? (
             <p className="mt-2 text-sm text-red-500">{state.message}</p>
