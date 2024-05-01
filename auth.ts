4936aef5f041705bdab53164d45d038b1c1fe92e
Wenting Y <ywtzoe@gmail.com>
2024-04-30 22:29:24 -0400

    Adding the Credentials provider.

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+	providers: [Credentials({})],
 });
 
 //Hashing converts a password into a fixed-length string of characters, which appears random, providing a layer of security even if the user's data is exposed.
