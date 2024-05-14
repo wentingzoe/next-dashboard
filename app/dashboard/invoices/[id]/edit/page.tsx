bdc22bc7075f42a4fc60fa98a370abbcc39deee5
Wenting Yong <wyong@orangerine.com>
2024-05-14 12:14:47 -0400

    Added Matedata to each page, which improve website SEO.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,8 +2,12 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 //Import a new function called fetchInvoiceById and pass the id as an argument.
 //Import fetchCustomers to fetch the customer names for the dropdown.
+export const metadata: Metadata = {
+  title: 'Editing Invoices ',
+};
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   // In order to fetch the invoice data, you need to call the fetchInvoice function with the id parameter
