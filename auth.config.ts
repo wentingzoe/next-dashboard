0fe376bc8fca0f3eca1bbb611e310a39a85a26d0
Wenting Yong <wyong@orangerine.com>
2024-05-14 09:54:40 -0400

    Added comments to understand the logic.

--- auth.config.ts
+++ auth.config.ts
@@ -7,16 +7,20 @@ export const authConfig = {
 // Protect dashboard route:
 	 callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
+      const isLoggedIn = !!auth?.user; 
+      //The !! operator is used to convert the value to a boolean
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false; 
+        // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
+        //Redirect login user to dashboard
       }
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [], 
+  // Add providers with an empty array for now
 } satisfies NextAuthConfig;
